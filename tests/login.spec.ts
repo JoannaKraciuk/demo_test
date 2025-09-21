17c901021fe09d7ae6fcfc8d2759d00f0a5130ed
Joanna Kraciuk <joanna.kraciuk@softiq.pl>
2025-09-21 22:04:51 +0200

    initial commit

---
+++ tests/login.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('User login to Demobank', () => {
+
+  test('successful login with correct credentials', async ({ page }) => {
+    // Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'Joanna10';
+    const userPassword = '10987654';
+    const expectedUserName = 'Jan Demobankowy';
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+
+
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
+  });
+
+  test('unsuccessful login with too short username', async ({ page }) => {
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'Joanna10';
+
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').click();
+
+    await expect(page.getByTestId('error-login-id')).toHaveText('identyfikator ma min. 8 znaków');
+  });
+
+  test('unsuccessful login with too short password', async ({ page }) => {
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'Joanna10';
+    const userPassword = '10987654';
+
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('password-input').blur();
+    
+
+    await expect(page.getByTestId('error-login-password')).toHaveText('hasło ma min. 8 znaków');
+  });
+
+});
